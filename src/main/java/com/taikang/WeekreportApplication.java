fecfcbc8ef36a278ce5e1d6c8c9a522d65b9200f
xingxf <13263397018@163.com>
2018-03-02 09:02:56 +0800

    Initial commit

---
+++ src/main/java/com/taikang/WeekreportApplication.java
@@ -0,0 +1,12 @@
+package com.taikang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeekreportApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeekreportApplication.class, args);
+	}
+}
